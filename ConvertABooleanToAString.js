271dd225d408905baba7ddc6b4bb48d0e769af58
Ekaterina Kulinich <kulinich.ekaterina.andreevna@gmail.com>
2024-02-18 21:33:23 +0100

    adding tasks

---
+++ ConvertABooleanToAString.js
@@ -0,0 +1,7 @@
+// Implement a function which convert the given boolean value into its string representation.
+// Note: Only valid inputs will be given.
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+
+function booleanToString(b){
+  return b.toString();
+}
